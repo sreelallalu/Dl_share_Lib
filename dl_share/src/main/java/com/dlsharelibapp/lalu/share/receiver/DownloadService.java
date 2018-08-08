9414124c179bdb510eeb7ab8a1986b1536f27b42
sreelal <Sreelal8026@gmail.com>
2018-08-08 10:33:54 +0530

    changes_27

--- dl_share/src/main/java/com/dlsharelibapp/lalu/share/receiver/DownloadService.java
+++ dl_share/src/main/java/com/dlsharelibapp/lalu/share/receiver/DownloadService.java
@@ -6,7 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.ResultReceiver;
-import android.support.v7.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 
 import com.dlsharelibapp.lalu.FileSave;
 
